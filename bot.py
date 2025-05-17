44d510852b65177afb3ca43f5c3c202dd4c73ef2
ismoil <ismoilumarzoda@gmail.com>
2025-05-17 08:06:51 +0500

    first commit

---
+++ bot.py
@@ -0,0 +1,29 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import Command
+from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, KeyboardButton, ReplyKeyboardMarkup
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from datetime import datetime
+from db import open_connection, init_models, close_connection
+
+TOKEN = '8144030905:AAEYkyyWUJEq9YZ7IgLLHlHpO_-8pVwbBK0'
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+
+
+
+
+
+async def main():
+    init_models()
+    print('ok')
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ db.py
@@ -0,0 +1,35 @@
+import sqlite3
+from datetime import datetime
+
+def open_connection():
+    sqliteconnection = sqlite3.connect('sql.db')
+    return sqliteconnection
+
+def close_connection(con, cur):
+    cur.close()
+    con.close()
+
+def init_models():
+    con = open_connection()
+    cur = con.cursor()
+    cur.execute(
+        '''
+    CREATE TABLE IF NOT EXISTS users(
+        telegram_id bigint primary key,
+        username varchar(50),
+        phone_number varchar(20),
+        index varchar(50)
+    );
+'''
+    )
+    cur.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS kargos(
+            id primary key autoincrements,
+            kod varchar(50),
+            vazn decimal(10,3),
+            adress text,
+            FOREIGN KEY (user_id) REFERENCES user(telegram_id) ON DELETE CASCADE
+        );
+'''
+    )
