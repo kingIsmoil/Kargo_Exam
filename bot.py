56d14456ddf899bba59e9139657e847a1bac3e60
Tillokhon <btillohon@gmail.com>
2025-05-17 08:23:55 +0500

    add hundler and state Kargo

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types, F
+from aiogram import Bot, Dispatcher, types, F,filters
 from aiogram.filters import Command
 from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, KeyboardButton, ReplyKeyboardMarkup
 from aiogram.fsm.state import StatesGroup, State
@@ -15,8 +15,57 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
+# Класи заказ равон кардан
+class Zakaz(StatesGroup):
+    kod_id=State()
+    vazn=State()
+    adress=State()
+# Листи заказ
+zakaz=[]
 
 
+# Канопкахо
+markub=ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Номнавис кардан"),KeyboardButton(text="Заказ равон кардан")],
+        [KeyboardButton(text="Филиалхои мо"),KeyboardButton(text="Оиди карго")]
+    ],
+    resize_keyboard=True
+)
+
+
+
+
+@dp.message(filters.Command("start"))
+async def start_pol(message:Message):
+    await message.answer("Ба Каргои Сомон мо хуш омадед барои хамкори ташаккур",reply_markup=markub)
+
+
+
+@dp.message(F.text=="Заказ равон кардан")
+
+async def zakaz_hundler(message:Message,state:FSMContext):
+    await state.set_state(Zakaz.kod_id)
+    await message.answer("Код id борро равон кунед: ")
+
+@dp.message(Zakaz.kod_id)
+async def kod_id_hundler(message:Message,state:FSMContext):
+    await state.update_data(kod_id=message.text)
+    await state.set_state(Zakaz.vazn)
+    await message.answer("Бори шумо чанд кг аст :")
+
+@dp.message(Zakaz.vazn)
+async def vazn_hundler(message:Message,state:FSMContext):
+    await state.update_data(vazn=message.text)
+    await state.set_state(Zakaz.adress)
+    await message.answer("Лутфан аддреси худро пурра равон кунед :")
+
+
+@dp.message(Zakaz.adress)
+
+async def adrez_hundler(message:Message,state:FSMContext):
+    zakaz= await state.update_data(addres=message.text)
+    await message.answer("Шумо бомувафакият закази худро равон кардед. Дар муддати 15 то 25 руз даставка мекунем")
 
 
 
