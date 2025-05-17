c2d0a56fe52479e5772527589c44d57195114f43
ismoil <ismoilumarzoda@gmail.com>
2025-05-17 09:56:38 +0500

    register

--- bot.py
+++ bot.py
@@ -15,14 +15,15 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
-# Класи заказ равон кардан
 class Zakaz(StatesGroup):
     kod_id=State()
     vazn=State()
     adress=State()
-# Листи заказ
 zakaz=[]
 
+class User(StatesGroup):
+    phone_number = State()
+    ind_id = State()
 
 # Канопкахо
 markub=ReplyKeyboardMarkup(
@@ -36,14 +37,40 @@ markub=ReplyKeyboardMarkup(
 
 
 
+
 @dp.message(filters.Command("start"))
-async def start_pol(message:Message):
+async def start_pol(message:Message,state:FSMContext):
     await message.answer("Ба Каргои Сомон мо хуш омадед барои хамкори ташаккур",reply_markup=markub)
+    con = open_connection()
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users WHERE telegram_id = ?", (message.from_user.id,))
+    user = cur.fetchone()
+    if not user:
+        await message.answer('vvedite nomer telephona')
+        await state.set_state(User.phone_number)
+    else:
+        await message.answer('viuje zaregistrirovani')
+        await message.answer('viberite knopki:', reply_markup=markub())
 
+@dp.message(User.phone_number)
+async def get_phone(message:Message,state:FSMContext):
+    await state.update_data(phone_number = message.text)
+    await message.answer('wwedite index')
+    await state.set_state(User.ind_id)
 
+@dp.message(User.ind_id)
+async def get_ind(message:Message, state:FSMContext):
+    await state.update_data(ind_id = message.text)
+    data = await state.get_data()
+    con = open_connection()
+    cur = con.cursor()
+    cur.execute("INSERT INTO users(telegram_id, username, phone_number, ind_id) VALUES (?, ? , ?, ?)", 
+            (message.from_user.id, message.from_user.full_name, data['phone_number'], data['ind_id']))
+    con.commit()
+    close_connection(con,cur)
+    await message.answer('vi zaregistrirovani')
 
 @dp.message(F.text=="Заказ равон кардан")
-
 async def zakaz_hundler(message:Message,state:FSMContext):
     await state.set_state(Zakaz.kod_id)
     await message.answer("Код id борро равон кунед: ")
@@ -62,13 +89,9 @@ async def vazn_hundler(message:Message,state:FSMContext):
 
 
 @dp.message(Zakaz.adress)
-
 async def adrez_hundler(message:Message,state:FSMContext):
-    zakaz= await state.update_data(addres=message.text)
+    zakaz= await state.update_data(adress=message.text)
     await message.answer("Шумо бомувафакият закази худро равон кардед. Дар муддати 15 то 25 руз даставка мекунем")
-
-
-
 
 
 
--- db.py
+++ db.py
@@ -29,7 +29,7 @@ def init_models():
             kod varchar(50),
             vazn decimal(10,3),
             adress text,
-            user_id int,
+            user_id bigint,
             FOREIGN KEY (user_id) REFERENCES users(telegram_id) ON DELETE CASCADE
         );
         '''
